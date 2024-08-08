424216cd799eea21ef681123080369f97bae42ea
Yevgeniy Python <itpython2023@gmail.com>
2024-08-08 18:49:07 +0300

    finish

--- query_2.py
+++ query_2.py
@@ -7,13 +7,13 @@ def execute_query(sql: str) -> list:
         cur.execute(sql)
         return cur.fetchall()
 
-# "Find the student with the highest average score in a singing subject. In this case, "jurisprudence"
+# "Find the student with the highest average score in a singing subject. In this case "subject_id = 2"
 
 sql = """
 SELECT ROUND(AVG(g.grade), 2) AS avg_grade, e.student_name, g.subject_id
 FROM grades as g
 LEFT JOIN students as e ON g.student_id = e.id
-WHERE g.subject_id = "jurisprudence"
+WHERE g.subject_id = 2
 GROUP BY g.subject_id, e.student_name 
 ORDER BY ROUND(AVG(g.grade), 2) DESC
 LIMIT 1;
